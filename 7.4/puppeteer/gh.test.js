1c1c25a61620d2fbc8f33bf571ba8fafcb9cd0ae
Julia Rubina <jl_rubina@mail.ru>
2023-09-04 21:20:11 +0300

    задание по puppeteer готово

--- 7.4/puppeteer/gh.test.js
+++ 7.4/puppeteer/gh.test.js
@@ -2,7 +2,6 @@ let page;
 
 beforeEach(async () => {
   page = await browser.newPage();
-  await page.goto("https://github.com/team");
 });
 
 afterEach(() => {
@@ -10,17 +9,22 @@ afterEach(() => {
 });
 
 describe("Github page tests", () => {
+
+  beforeEach(async () => {
+    await page.goto("https://github.com/team");
+  });
+  
   test("The h1 header content'", async () => {
     const firstLink = await page.$("header div div a");
     await firstLink.click();
     await page.waitForSelector('h1');
     const title2 = await page.title();
-    expect(title2).toEqual('GitHub: Where the world builds software · GitHub');
+    expect(title2).toEqual("GitHub for teams · Build like the best teams on the planet · GitHub", {timeout: 60000});
   });
 
   test("The first link attribute", async () => {
     const actual = await page.$eval("a", link => link.getAttribute('href') );
-    expect(actual).toEqual("#start-of-content");
+    expect(actual).toEqual("#start-of-content", {timeout: 60000});
   });
 
   test("The page contains Sign in button", async () => {
@@ -29,6 +33,27 @@ describe("Github page tests", () => {
       visible: true,
     });
     const actual = await page.$eval(btnSelector, link => link.textContent);
-    expect(actual).toContain("Sign up for free")
+    expect(actual).toContain("Get started with Team", {timeout: 60000})
+  });
+});
+
+describe("Another github page tests", () => {
+  
+  test("The Features page", async () => {
+    await page.goto("https://github.com/features");
+    const titleFeatures = await page.title();
+    expect(titleFeatures).toEqual("Features | GitHub · GitHub", {timeout: 60000});
+  });
+
+  test("About page", async () => {
+    await page.goto("https://github.com/about");
+    const titleCodespace = await page.title();
+    expect(titleCodespace).toEqual('About · GitHub', {timeout: 60000})
+  });
+
+  test("The pricing page", async () => {
+    await page.goto("https://github.com/pricing");
+    const titlePricing = await page.title();
+    expect(titlePricing).toEqual('Pricing · Plans for every developer · GitHub', {timeout: 60000})
   });
 });
